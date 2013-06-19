bc517f55261eb5a571684ff0971baf1b8d9cbbfd
charsyam <charsyam@gmail.com>
2013-06-20 00:43:39 +0900

    add default sls.c

--- day2/02/sls.c
+++ day2/02/sls.c
@@ -5,29 +5,18 @@
 #include    <unistd.h>
 
 int main(int argc, char *argv[]) {
-    DIR *dir = opendir(argv[1]);
+    DIR *dir = opendir(".");
     struct dirent *dirInfo = NULL;
 
-    FILE *fp = fopen("result.xml","w");
-    if (fp == NULL) {
-        fprintf(fp, "File open Error: %s\n", "result.xml");
-        return 1;
-    }
-
-    fprintf(fp, "<files>\n");
     while((dirInfo = readdir(dir)) != NULL) {
-        fprintf(fp, "\t<file>\n");
-        fprintf(fp, "\t\t<type>%d</type>\n", dirInfo->d_type);
-        fprintf(fp, "\t\t<name>%s</name>\n", dirInfo->d_name);
-
+        printf("(%d)%s",dirInfo->d_type, dirInfo->d_name);
         if ((dirInfo->d_type & DT_REG) == DT_REG) {
             struct stat buf;
             stat(dirInfo->d_name, &buf);
-            fprintf(fp, "\t\t<size>%d</size>\n", buf.st_size);
+            printf(" size(%d)", buf.st_size);
         }
-        fprintf(fp, "\t</file>\n");
+        printf("\n");
     }
-    fprintf(fp, "</files>\n");
     
     closedir(dir); 
 
