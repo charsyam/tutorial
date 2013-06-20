61873ceeb1df87fd084ddd7b77cfcdd32fe66955
CharSyam <charsyam@gmail.com>
2013-06-20 15:17:39 +0000

    tcp server

---
+++ day2/07/tcp/server.c
@@ -0,0 +1,56 @@
+/* socket header file */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h> /* AF_INET socket */
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+
+#define UDPPORT     2001    /* UDP PORT number */
+
+int main(int argc, char *argv[])
+{
+    int serverLen;  /* address struct length */
+    int serverFd;   /* server socket */
+    struct sockaddr_in serverINETAddress;   /* server INET address */
+    struct sockaddr* serverSockAddrPtr; /* server adress pointer */
+    char udp_buf[] = "testing..";
+    int numbytes;
+
+
+    serverLen = sizeof(struct sockaddr); /* struct length */    
+    serverSockAddrPtr = (struct sockaddr *)&serverINETAddress;
+
+    bzero(&(serverINETAddress.sin_zero),8);
+    serverINETAddress.sin_family = AF_INET;      /* host byte order */
+    serverINETAddress.sin_port = htons(UDPPORT);  /* short, network byte order */
+    serverINETAddress.sin_addr.s_addr = htonl(INADDR_ANY); /* BROADCAST */
+
+    if ((serverFd = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+    {
+        perror("socket");
+        exit(1);
+    }
+
+    bind(serverFd, serverSockAddrPtr, serverLen);
+    listen(serverFd,100);
+
+    char buffer[4096];
+    char s[1024];
+    while (1) {
+        int len = sizeof(struct sockaddr_in);
+        struct sockaddr_in in;
+        int newfd = accept(serverFd, (struct sockaddr *)&in, &len);
+        numbytes = recv(newfd, buffer, sizeof(buffer), 0);
+        buffer[numbytes] = 0;
+        printf("Receive: %s(%d)", buffer, numbytes);
+        inet_ntop(AF_INET, &(in.sin_addr), s, sizeof(s));
+        printf("len: %d, addr: %s\n", len, s);
+        close(newfd);
+    }
+
+    close(serverFd);
+    return 0;
+}
